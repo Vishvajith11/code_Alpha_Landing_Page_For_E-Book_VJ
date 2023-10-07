eed1ed7e47c2037a6adaf08338f2c86842681ff5
Vishvajith11 <147233793+Vishvajith11@users.noreply.github.com>
2023-10-07 14:28:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function submitform() {
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("book").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("phone").value = "";
+
+    alert("Data saved successfully!");
+}
